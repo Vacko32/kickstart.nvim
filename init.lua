bcf0b6983299f050bcb30de278e3bcdfd7724825
Martin Vaculik <mvaculik4@gmail.com>
2025-09-30 04:34:38 +0200

    added back mm for terminal insert quit

--- init.lua
+++ init.lua
@@ -90,6 +90,7 @@ vim.keymap.set('i', 'jj', '<Esc>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 vim.keymap.set('v', 'mm', '<Esc>')
+vim.keymap.set('t', 'mm', '<C-\\><C-n>')
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
